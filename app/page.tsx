316759d1df9e5e5ef6c45db9e83709b24ae171a3
Alexandru Galanton <alexarhitectul@gmail.com>
2026-03-06 17:10:20 +0200

    chore: redirect home to auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return (
-    <main style={{ padding: 40 }}>
-      <h1>Scorebet is live</h1>
-      <p>Deployment works.</p>
-      <p>
-        <a href="/auth">Go to auth</a>
-      </p>
-    </main>
-  );
+  redirect("/auth");
 }
